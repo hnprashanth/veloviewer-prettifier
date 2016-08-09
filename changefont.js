e567ad323bbd10d545090ee0304218c3007305ef
Prashanth HN <prashanth@prashanth.net>
2016-08-09 10:25:23 +0530

    Initial Commit

---
+++ changefont.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	$('a').css('color', '#000000');
+	$('a').css('font-size', '16px');
+	$('table').css('font-size', '16px');
+	$('.navbar-inverse .nav > li > a').css('color', '#999999');
+});
